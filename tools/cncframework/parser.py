7b6939e3cd09d9cedb55c2fdf424965dda668ef7
Nick Vrvilo <Nick.Vrvilo@rice.edu>
2015-12-29 21:47:28 -0600

    Improve error reporting by CnC spec file parser

--- tools/cncframework/parser.py
+++ tools/cncframework/parser.py
@@ -2,40 +2,6 @@ from pyparsing import *
 
 
 ##################################################
-# PARSER HELPERS
-
-class InjectToken(Token):
-    """Matches nothing and returns the provided string as a new token.
-       Useful for tagging groups with an identifying string."""
-    def __init__(self, strToken):
-        super(InjectToken,self).__init__()
-        self.mayReturnEmpty = False
-        self.mayIndexError = False
-        self.returnString = strToken
-    def parseImpl(self, instring, loc, doActions=True):
-        return loc, self.returnString
-
-def rep1sep(repeated, separator=","):
-    """Repeated pattern with ignored separator. Similar to
-       pyparsing.delimitedList, but this is non-empty."""
-    sep = Suppress(separator)
-    return Group(repeated + ZeroOrMore(sep + repeated))
-
-def notSpace(expr):
-    """Prevents wrapped parser from matching only whitespace.
-       Only when whitespace can be matched (e.g. CharsNotIn)."""
-    return Optional(White()).suppress() + expr
-
-def joined(parser, sep=""):
-    """Join the resulting tokens from the parser into a single token"""
-    return Combine(parser, joinString=sep, adjacent=False)
-
-def kind(tokStr):
-    """Inject a string literal to denote the kind of the current result."""
-    return InjectToken(tokStr)('kind')
-
-
-##################################################
 # ERROR HELPERS
 
 closestMatchError = None
@@ -79,6 +45,50 @@ def parserErrorWrapper(parser, specPath):
 
 
 ##################################################
+# PARSER HELPERS
+
+class InjectToken(Token):
+    """Matches nothing and returns the provided string as a new token.
+       Useful for tagging groups with an identifying string."""
+    def __init__(self, strToken):
+        super(InjectToken,self).__init__()
+        self.mayReturnEmpty = False
+        self.mayIndexError = False
+        self.returnString = strToken
+    def parseImpl(self, instring, loc, doActions=True):
+        return loc, self.returnString
+
+def rep1sep(repeated, separator=","):
+    """Repeated pattern with ignored separator. Similar to
+       pyparsing.delimitedList, but this is non-empty."""
+    sep = Suppress(separator)
+    return Group(repeated + ZeroOrMore(sep + repeated))
+
+def notSpace(expr):
+    """Prevents wrapped parser from matching only whitespace.
+       Only when whitespace can be matched (e.g. CharsNotIn)."""
+    return Empty() + expr
+
+def joined(parser, sep=""):
+    """Join the resulting tokens from the parser into a single token"""
+    return Combine(parser, joinString=sep, adjacent=False)
+
+def kind(tokStr):
+    """Inject a string literal to denote the kind of the current result."""
+    return InjectToken(tokStr)('kind')
+
+def closing(tok):
+    """Tries to match a closing token (e.g., a closing bracket),
+       and gives a failure if it is not found."""
+    return tok | failure("Expected closing '{0}'".format(tok))
+
+def delimiter(tok):
+    """Tries to match a delimiter token (e.g., a semicolon),
+       and gives a failure if it is not found."""
+    return tok | failure("Expected delimiter: '{0}'".format(tok))
+
+
+##################################################
 # C-STYLE VARIABLES AND EXPRESSIONS
 # (specify step I/O relationships for items/steps)
 
@@ -87,7 +97,9 @@ cVar = Word("_"+alphas, "_"+alphanums)
 
 # C-style expressions
 cExpr = Forward() # forward-declaration
-cSubExpr = "(" + cExpr + ")" | "[" + cExpr + "]" | "{" + cExpr + "}"
+cSubExpr = ( "(" + cExpr + closing(")")
+           | "[" + cExpr + closing("]")
+           | "{" + cExpr + closing("}") )
 cExpr <<= ZeroOrMore(CharsNotIn("()[]{}") | cSubExpr) # concrete definition
 cExpr.leaveWhitespace()
 cTopExpr = joined(notSpace(ZeroOrMore(CharsNotIn("()[]{},") | cSubExpr)))
@@ -109,14 +121,14 @@ cTypeStars = joined(ZeroOrMore(cStar))
 
 cType = cTypeBase('baseType') + cTypeStars('stars')
 
-cArraySuffix = "[" + cExpr('arraySize') + "]"
+cArraySuffix = "[" + cExpr('arraySize') + closing("]")
 
 ##################################################
 # Context struct fields declaration
 # (used to add custom parameters to the context)
 
 cncContext = CaselessKeyword("$context").suppress() + Suppress("{")\
-           + cExpr('fields') + Suppress("}") + Suppress(";")
+           + cExpr('fields') + closing("}").suppress() + delimiter(";").suppress();
 
 
 ##################################################
@@ -144,12 +156,14 @@ def deprecatedRangeSyntaxWarning(s, loc, tok):
     print "\t", ltxt
     print "\t(Please the $range or $rangeTo function instead.)\n"
 
-oldRangeExpr = "{" + rangeSafeExpr('start') + ".." + rangeSafeExpr('end') + "}"
+oldRangeExpr = ( "{" + rangeSafeExpr('start') + delimiter("..")
+               + rangeSafeExpr('end') + closing("}") )
 oldRangeExpr.addParseAction(deprecatedRangeSyntaxWarning)
 
 # Newer-style range functions
 rangeFn = CaselessKeyword("$rangeTo")('inclusive') | CaselessKeyword("$range")
-rangeExpr = rangeFn + "(" + Optional(scalarExpr('start') + ",") + scalarExpr('end') + ")"
+rangeExpr = ( rangeFn + delimiter("(") + Optional(scalarExpr('start')
+            + ",") + scalarExpr('end') + closing(")") )
 
 rangedTC = Group(kind('RANGED') + (rangeExpr | oldRangeExpr))
 
@@ -160,9 +174,9 @@ rangedTC = Group(kind('RANGED') + (rangeExpr | oldRangeExpr))
 
 attrKey = cVar
 attrVal = cTopExpr
-attrPair = Group(attrKey + Suppress(":") + attrVal)
+attrPair = Group(attrKey + delimiter(":").suppress() + attrVal)
 attrDictSep = Suppress(",")
-attrDict = Suppress("{") + Optional(Dict(attrPair + ZeroOrMore(attrDictSep + attrPair))) + Suppress("}")
+attrDict = Suppress("{") + Optional(Dict(attrPair + ZeroOrMore(attrDictSep + attrPair))) + closing("}").suppress()
 optAttrs = Optional(attrDict)
 
 
@@ -170,18 +184,19 @@ optAttrs = Optional(attrDict)
 # TAGS
 # (keys for items and tags for steps)
 
-tagDecl = rep1sep(cVar) | unitExpr
-tagExpr = unitExpr | rep1sep(rangedTC | scalarTC)
-scalarTagExpr = rep1sep(scalarExpr)
+tagDecl = rep1sep(cVar | failure("Expected tag identifier")) | unitExpr
+tagExpr = unitExpr | rep1sep(rangedTC | scalarTC | failure("Expected tag expression"))
+scalarTagExpr = rep1sep(scalarExpr | failure("Expected tag expression"))
 
 
 ##################################################
 # ITEM INSTANCE REFERENCE
 # (used in step input/output relationships)
 
-itemRef = (Group("[" + kind('ITEM') + Optional(cVar('binding') + "@") \
-                + cVar('collName') + ":" + tagExpr('key') + "]")
-           | failure("Expected item reference"))
+itemRef = (Group("[" + kind('ITEM') + Optional(cVar('binding') + "@")
+                + cVar('collName') + delimiter(":").suppress()
+                + tagExpr('key') + closing("]"))
+           | failure("Expected input item reference"))
 
 
 ##################################################
@@ -191,21 +206,23 @@ itemRef = (Group("[" + kind('ITEM') + Optional(cVar('binding') + "@") \
 # mappings from virtual to concrete item collections can be
 # specified by a function name, or inline as a tag expression
 externalMapping = CaselessKeyword("using") + cVar('funcName')
-inlineMapping = ":" + scalarTagExpr('keyFunc')
+inlineMapping = delimiter(":") + scalarTagExpr('keyFunc')
 mappingFunction = externalMapping | inlineMapping
-itemMapping = cVar('targetCollName') + mappingFunction
+itemMapping = ( cVar('targetCollName') + mappingFunction
+              | failure("Expected item-key mapping function") )
 
 cTypedVar = cType('type') + cVar('collName') + Optional(cArraySuffix)('vecSuffix')
-itemDecl = Group("[" + cTypedVar + ":" + tagDecl('key') \
-                + Optional("=" + itemMapping)('virtualMapping') + "]" + ";")
+itemDecl = Group("[" + cTypedVar + delimiter(":") + tagDecl('key')
+                + Optional("=" + itemMapping)('virtualMapping')
+                + closing("]") + delimiter(";"))
 
 
 ##################################################
 # STEP INSTANCE REFERENCE
 # (used in step output relationships)
 
-stepRef = Group("(" + kind('STEP') + cVar('collName') \
-               + ":" + tagExpr('tag') + ")")
+stepRef = Group("(" + kind('STEP') + cVar('collName')
+               + delimiter(":") + tagExpr('tag') + closing(")"))
 
 
 ##################################################
@@ -216,13 +233,15 @@ stepRef = Group("(" + kind('STEP') + cVar('collName') \
 kwIf = CaselessKeyword("$if") + kind('IF')
 kwElse = CaselessKeyword("$else") + kind('ELSE')
 kwWhen = CaselessKeyword("$when") + kind('IF')
-instanceRef = itemRef | stepRef | failure("Expected step or item reference")
+instanceRef = itemRef | stepRef | failure("Expected output step/item reference")
 
 def condBlock(ref):
-    refBlock = "{" + rep1sep(ref)('refs') + "}"
-    cond = "(" + cExpr('cond') + ")"
-    return Group(kwIf + cond + refBlock) + Optional(Group(kwElse + refBlock)) \
-         | Group(Group(ref)('refs') + (kwWhen + cond | kind('ALWAYS')))
+    refBlock = ( "{" + rep1sep(ref)('refs') + closing("}")
+               | failure("Expecting a block of instance references") )
+    cond = ( "(" + cExpr('cond') + closing(")")
+           | failure("Expected a conditional") )
+    return ( Group(kwIf + cond + refBlock) + Optional(Group(kwElse + refBlock))
+           | Group(Group(ref)('refs') + (kwWhen + cond | kind('ALWAYS'))) )
 
 condItemRefs = rep1sep(condBlock(itemRef))
 condInstanceRefs = rep1sep(condBlock(instanceRef))
@@ -237,16 +256,18 @@ initFnName =  CaselessKeyword("$init") | CaselessKeyword("$initialize").suppress
 
 # Helpers for parsing references to other items/steps
 stepName = cVar | initFnName | CaselessKeyword("$finalize")
-stepDecl = Group("(" + stepName('collName') + ":" + tagDecl('tag') + ")")
+stepDecl = Group("(" + stepName('collName') + delimiter(":")
+                + tagDecl('tag') + closing(")"))
 
 stepRelation = (Group(stepDecl('step') \
-                     + Optional("<-" + condItemRefs('inputs')) \
-                     + Optional("->" + condInstanceRefs('outputs')) + ";")
+                     + Optional(delimiter("<-") + condItemRefs('inputs')) \
+                     + Optional(delimiter("->") + condInstanceRefs('outputs'))
+                     + delimiter(";"))
                 | failure("Expected step function declaration"))
 
 
 ##################################################
-# CNC GRAPH SPEC
+# CnC GRAPH SPEC
 # (parses an entire spec file)
 
 graphCtx = Optional(joined(cncContext))
@@ -260,12 +281,14 @@ def parseGraphFile(specPath):
 
 
 ##################################################
-# CNC TUNING SPEC
+# CnC TUNING SPEC
 # (parses an entire tuning spec file)
 
-itemTune = Group("[" + cVar('collName') + "]" + ":" + attrDict('attrs') + ";")
-inputTune = Literal("<-") + "[" + cVar('inputName') + "]"
-stepTune = Group("(" + cVar('collName') + ")" + Optional(inputTune) + ":" + attrDict('attrs') + ";")
+itemTune = Group("[" + cVar('collName') + closing("]") + delimiter(":")
+                + attrDict('attrs') + delimiter(";"))
+inputTune = delimiter("<-") + "[" + cVar('inputName') + closing("]")
+stepTune = Group("(" + cVar('collName') + closing(")") + Optional(inputTune)
+                + delimiter(":") + attrDict('attrs') + delimiter(";"))
 
 itemTunings = ZeroOrMore(itemTune)('itemTunings')
 stepTunings = ZeroOrMore(stepTune)('stepTunings')
