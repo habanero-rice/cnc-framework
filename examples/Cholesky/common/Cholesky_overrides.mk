e71179f4ee7a4a049627a1ac96502a4a80a6c5a2
Nick Vrvilo <Nick.Vrvilo@rice.edu>
2017-07-07 15:55:34 -0500

    Error-out if user tries to build Cholesky/common

---
+++ examples/Cholesky/common/Cholesky_overrides.mk
@@ -0,0 +1,2 @@
+# alert users that they can't build the files in this directory
+$(error This subdirectory (common) does not contain a complete project! Use the files in the generated_input or file_input subdirectories instead. See the README for more details)
